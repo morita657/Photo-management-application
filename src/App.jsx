99ab17ab2d21f9ffa357252f215ab08a9451fa3c
morita657 <morita657@gmail.com>
2018-05-12 13:01:21 +0900

    Implement SinglePhoto component to switch view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Title from "./Title.jsx";
 import AllPhoto from "./AllPhoto.jsx";
+import SinglePhoto from "./SinglePhoto.jsx";
 
 class App extends Component {
     constructor(props) {
@@ -9,17 +10,40 @@ class App extends Component {
         this.state = {
             photos: ["http://via.placeholder.com/350x150", "http://via.placeholder.com/350x150", "http://via.placeholder.com/350x150"],
             selectedPhoto: "",
-            id: 0
+            id: 0,
+            view: "ShowAllPhoto"
         }
+        this.showAllphoto = this.showAllphoto.bind(this);
+        this.showSinglephoto = this.showSinglephoto.bind(this);
+        this.showSelectedPhoto = this.showSelectedPhoto.bind(this);
+
     }
     showAllphoto() {
-        this.currentView = 'ShowAllPhoto'
+        return this.setState({ view: 'ShowAllPhoto' });
     };
+    showSinglephoto() {
+        console.log('hi');
+        // return;
+        return this.setState({ view: 'ShowSinglePhoto' });
+    };
+    showSelectedPhoto(id) {
+        const newState = this.setState({ id });
+        return newState;
+    }
+    get currentView() {
+        if (this.state.view === "ShowAllPhoto") {
+            return (<AllPhoto photos={this.state.photos} showSelectedPhoto={this.showSelectedPhoto} />);
+        }
+        if (this.state.view === "ShowSinglePhoto") {
+            return (<SinglePhoto />);
+        }
+    }
     render() {
         return (
             <div>
-                <Title />
-                <AllPhoto photos={this.state.photos} showSelectedPhoto={this.showSelectedPhoto} />
+                <Title showSinglephoto={this.showSinglephoto} />
+                {this.currentView}
+                {/* <AllPhoto photos={this.state.photos} showSelectedPhoto={this.showSelectedPhoto} /> */}
             </div>
         );
     }
