902dbca06a9e17cece283e0bc16310c4a8cb5204
morita657 <morita657@gmail.com>
2018-05-12 12:41:32 +0900

    Implement title component

---
+++ src/Title.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Title extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>INSTA-HACK - The Hacky Instagram!</h1>
+            </div>
+        );
+    }
+}
+export default Title;
