b28900dca7ac827e4077a25516e32a091a0ef0eb
morita657 <morita657@gmail.com>
2018-05-13 20:04:37 +0900

    Connect with reducer on SinglePhoto

--- src/SinglePhoto.jsx
+++ src/SinglePhoto.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import moment from "moment";
+import { connect } from "react-redux";
 
 class SinglePhoto extends Component {
-    constructor(props) {
-        super(props);
-    }
     get timeConvert() {
-        const date = moment(this.props.photo["timestamp"]);
-        console.log(this.props.photo["timestamp"]);
+        const date = moment(this.props.photos[this.props.id]["timestamp"]);
+        // console.log(this.props.photo["timestamp"]);
         const timeFormat = date.utc().format('YYYY-MM-DD');
-        console.log(timeFormat);
+        // console.log(timeFormat);
         return date.from(moment("2018-05-12"));
     }
     render() {
-
         return (
             <div>
-                <img id="single-img" src={this.props.photo["photoPath"]} />
-                <p>username: <span>{this.props.photo["username"]}</span></p>
+                <img id="single-img" src={this.props.photos[this.props.id]["photoPath"]} />
+                <p>username: <span>{this.props.photos[this.props.id]["username"]}</span></p>
                 <p>{this.timeConvert}</p>
                 <button onClick={() => this.props.deletePhoto(this.props.id)}>DELETE</button>
             </div>
         );
     }
 }
-export default SinglePhoto;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePhoto: (e) => {
+            return dispatch({ type: "DeletePhoto", id: e });
+        }
+    };
+};
+const mapStateToProps = (state) => {
+    console.log('SinglePhoto state', state);
+    return ({ view: state.view, photos: state.photos, id: state.id })
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SinglePhoto);
