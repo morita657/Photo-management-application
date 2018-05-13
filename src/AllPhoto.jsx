2e5ed86efdcd79dd5f655a407659103cdd84001a
morita657 <morita657@gmail.com>
2018-05-13 21:03:06 +0900

    Mount component not working

--- src/AllPhoto.jsx
+++ src/AllPhoto.jsx
@@ -12,6 +12,20 @@ class AllPhoto extends Component {
         this.showSinglephoto();
         return newState;
     }
+    // getPhotos() {
+    //     const storage = localStorage.getItem("insta-hack");
+    //     if (storage !== null) {
+    //         // return this.setState({ photos: JSON.parse(storage) });
+    //         // const raw = JSON.parse(storage);
+    //         // const newState = this.setState({ photos: raw });
+    //         // return newState;
+    //         return JSON.parse(storage);;
+    //         // this.props.photos = JSON.parse(storage);
+    //     }
+    // }
+    // componentDidMount() {
+    //     return this.getPhotos();
+    // }
     render() {
         return (
             <div>
@@ -27,8 +41,21 @@ const mapDispatchToProps = (dispatch) => {
         showSelectedPhoto: (event) => {
             return dispatch({ type: "ShowSinglePhoto", id: event, });
         },
+        getPhotos: () => {
+            console.log(this.state.photos);
+
+            return dispatch({ photos: this.state.photos });
+        }
     };
 };
+// After dispatching, still not working...
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         getPhotos: () => {
+//             return dispatch({ photos: this.state.photos });
+//     }
+//     // };
+// };
 const mapStateToProps = (state) => {
     return ({ view: state.view, photos: state.photos });
 };
--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,6 @@ import SinglePhoto from "./SinglePhoto.jsx";
 import UploadPhoto from "./UploadPhoto.jsx";
 import { connect } from "react-redux";
 class App extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     // this.getPhotos = this.getPhotos.bind(this);
-    //     // this.showAllphoto = this.showAllphoto.bind(this);
-    //     // this.showSinglephoto = this.showSinglephoto.bind(this);
-    //     // this.showSelectedPhoto = this.showSelectedPhoto.bind(this);
-    //     // this.showUploadPhoto = this.showUploadPhoto.bind(this);
-    //     // this.uploadPhoto = this.uploadPhoto.bind(this);
-    //     // this.deletePhoto = this.deletePhoto.bind(this);
-    // }
-
-    // showUploadPhoto() {
-    //     return this.setState({ view: 'UploadPhoto' });
-    // };
-    // uploadPhoto(file, name) {
-    //     const storage = localStorage.getItem("insta-hack");
-    //     const photoInfo = { "username": name, "photoPath": file, "timestamp": new Date() };
-    //     if (storage === null) {
-    //         localStorage.setItem("insta-hack", JSON.stringify([photoInfo]));
-    //     }
-    //     else {
-    //         // convert storage value into array and push new value
-    //         const updatedData = JSON.parse(storage)
-    //         updatedData.push(photoInfo);
-    //         localStorage.setItem("insta-hack", JSON.stringify(updatedData));
-    //     }
-    //     return this.setState({ photos: JSON.parse(storage) });
-    // }
-    // deletePhoto(id) {
-    //     const storage = localStorage.getItem("insta-hack");
-    //     const raw = JSON.parse(storage);
-    //     raw.splice(id, 1);
-    //     localStorage.setItem("insta-hack", JSON.stringify(raw));
-    //     this.showAllphoto();
-    // }
     get currentView() {
         if (this.props.view === "ShowAllPhoto") {
             return (<AllPhoto />);
@@ -62,6 +27,6 @@ class App extends Component {
     }
 }
 const mapStateToProps = (state) => {
-    return ({ view: state.view, });
+    return ({ view: state.view });
 };
-export default connect(mapStateToProps, )(App);
+export default connect(mapStateToProps)(App);
