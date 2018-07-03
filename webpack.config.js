286d7cb4bc3afe606fb8f1f421079fbf6ec43f76
morita657 <morita657@gmail.com>
2018-07-03 14:03:29 +0900

    Install gh-pages-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require("path");
+const GhPagesWebpackPlugin = require("gh-pages-webpack-plugin");
 
 module.exports = {
-    entry: "./index.jsx",
-    output: { path: path.resolve(__dirname, "public"), filename: "bundle.js" },
-    module: {
-        loaders: [
-            {
-                test: /.jsx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-                query: {
-                    presets: ["react", "stage-2"],
-                },
-            },
-            {
-                test: /.css?$/,
-                loader: ["style-loader", "css-loader"],
-                exclude: /node_modules/,
-            },
-        ],
-    },
+  entry: "./index.jsx",
+  output: { path: path.resolve(__dirname, "public"), filename: "bundle.js" },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          presets: ["react", "stage-2"]
+        }
+      },
+      {
+        test: /.css?$/,
+        loader: ["style-loader", "css-loader"],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new GhPagesWebpackPlugin({
+      path: "./public"
+    })
+  ]
 };
